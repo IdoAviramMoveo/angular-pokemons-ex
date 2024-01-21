e0e72d241d790ba9b88b87ce1eb36f82042740a3
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-21 17:18:50 +0200

    Added navbar and google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'The Pokemons App';
+  currentRoute: string = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.url;
+      });
+  }
+
+  showPokemonLogo(): boolean {
+    return (
+      this.currentRoute.includes('/pokemons') ||
+      this.currentRoute.startsWith('/pokemon/')
+    );
+  }
+
+  showGoogleMapsLogo(): boolean {
+    return this.currentRoute.includes('/my-map');
+  }
 }
--- src/app/components/my-map/my-map.component.ts
+++ src/app/components/my-map/my-map.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-my-map',
   templateUrl: './my-map.component.html',
-  styleUrl: './my-map.component.scss'
+  styleUrls: ['./my-map.component.scss'],
 })
-export class MyMapComponent {
+export class MyMapComponent implements OnInit {
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
 
+  ngOnInit() {
+    let loader = new Loader({
+      apiKey: 'AIzaSyAgORTeuVwfKVXJl7DoefqL5KRRPYRvgnE',
+      version: 'weekly',
+    });
+
+    loader.load().then(() => {
+      new google.maps.Map(this.mapContainer.nativeElement, {
+        center: { lat: 51.233334, lng: 6.78333 },
+        zoom: 6,
+      });
+    });
+  }
 }
+
+//AIzaSyAgORTeuVwfKVXJl7DoefqL5KRRPYRvgnE
