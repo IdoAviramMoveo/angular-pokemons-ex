fa81373ed60a0fe15bea107116fa6d032c98f81d
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-21 18:33:54 +0200

    Added marker and autocomplete

--- src/app/components/my-map/my-map.component.ts
+++ src/app/components/my-map/my-map.component.ts
@@ -4,24 +4,64 @@ import { Loader } from '@googlemaps/js-api-loader';
 @Component({
   selector: 'app-my-map',
   templateUrl: './my-map.component.html',
-  styleUrls: ['./my-map.component.scss'],
+  styleUrl: './my-map.component.scss',
 })
 export class MyMapComponent implements OnInit {
   @ViewChild('mapContainer') mapContainer!: ElementRef;
+
+  private map: google.maps.Map;
+  private marker: google.maps.Marker;
+  private autocomplete: google.maps.places.Autocomplete;
 
   ngOnInit() {
     let loader = new Loader({
       apiKey: 'AIzaSyAgORTeuVwfKVXJl7DoefqL5KRRPYRvgnE',
       version: 'weekly',
+      libraries: ['places'],
     });
 
     loader.load().then(() => {
-      new google.maps.Map(this.mapContainer.nativeElement, {
-        center: { lat: 51.233334, lng: 6.78333 },
-        zoom: 6,
+      const position = { lat: 32.06462, lng: 34.77163 };
+
+      this.map = new google.maps.Map(this.mapContainer.nativeElement, {
+        center: position,
+        zoom: 14,
+        mapId: '84b113c4aa93ca25',
+      });
+
+      this.marker = new google.maps.Marker({
+        position: position,
+        map: this.map,
+        title: 'Moveo',
+      });
+
+      const input = document.getElementById('addressInput') as HTMLInputElement;
+      this.autocomplete = new google.maps.places.Autocomplete(input);
+
+      this.autocomplete.addListener('place_changed', () => {
+        const place = this.autocomplete.getPlace();
+        if (!place.geometry) {
+          console.error('No details available for input: ', place.name);
+          return;
+        }
+
+        this.map.setCenter(place.geometry.location);
+        this.map.setZoom(14);
+
+        this.marker.setPosition(place.geometry.location);
+        this.marker.setTitle(place.name);
       });
     });
   }
-}
 
-//AIzaSyAgORTeuVwfKVXJl7DoefqL5KRRPYRvgnE
+  addMarkerFromInput() {
+    const place = this.autocomplete.getPlace();
+    if (place.geometry) {
+      this.map.setCenter(place.geometry.location);
+      this.map.setZoom(14);
+
+      this.marker.setPosition(place.geometry.location);
+      this.marker.setTitle(place.name);
+    }
+  }
+}
