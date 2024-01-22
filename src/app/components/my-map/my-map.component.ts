e6b779d644175e918bcc757c7de42727eb7b88cc
IdoAviramMoveo <idoa@moveo.co.il>
2024-01-22 10:43:46 +0200

    Added styles and markers

--- src/app/components/my-map/my-map.component.ts
+++ src/app/components/my-map/my-map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 
+import { styledMap } from '../../../constants/styled-map';
+
 @Component({
   selector: 'app-my-map',
   templateUrl: './my-map.component.html',
@@ -10,48 +12,50 @@ export class MyMapComponent implements OnInit {
   @ViewChild('mapContainer') mapContainer!: ElementRef;
 
   private map: google.maps.Map;
-  private marker: google.maps.Marker;
+  private markers: google.maps.Marker[] = [];
   private autocomplete: google.maps.places.Autocomplete;
 
+  private isCustomStyleApplied: boolean = false;
+  private customStyles: google.maps.MapTypeStyle[] = styledMap;
+  private defaultStyles: google.maps.MapTypeStyle[] = [];
+
   ngOnInit() {
-    let loader = new Loader({
+    const loader = new Loader({
       apiKey: 'AIzaSyAgORTeuVwfKVXJl7DoefqL5KRRPYRvgnE',
       version: 'weekly',
       libraries: ['places'],
     });
 
     loader.load().then(() => {
-      const position = { lat: 32.06462, lng: 34.77163 };
+      const defaultPosition = { lat: 32.06462, lng: 34.77163 };
 
       this.map = new google.maps.Map(this.mapContainer.nativeElement, {
-        center: position,
+        center: defaultPosition,
         zoom: 14,
-        mapId: '84b113c4aa93ca25',
+        styles: this.defaultStyles,
       });
 
-      this.marker = new google.maps.Marker({
-        position: position,
-        map: this.map,
-        title: 'Moveo',
-      });
+      this.addMarker(defaultPosition, 'Default Location');
 
       const input = document.getElementById('addressInput') as HTMLInputElement;
       this.autocomplete = new google.maps.places.Autocomplete(input);
 
       this.autocomplete.addListener('place_changed', () => {
-        const place = this.autocomplete.getPlace();
-        if (!place.geometry) {
-          console.error('No details available for input: ', place.name);
-          return;
-        }
-
-        this.map.setCenter(place.geometry.location);
-        this.map.setZoom(14);
-
-        this.marker.setPosition(place.geometry.location);
-        this.marker.setTitle(place.name);
+        this.addMarkerFromInput();
       });
     });
+  }
+
+  addMarker(
+    position: google.maps.LatLng | google.maps.LatLngLiteral,
+    title: string
+  ): void {
+    const marker = new google.maps.Marker({
+      position,
+      map: this.map,
+      title,
+    });
+    this.markers.push(marker);
   }
 
   addMarkerFromInput() {
@@ -60,8 +64,18 @@ export class MyMapComponent implements OnInit {
       this.map.setCenter(place.geometry.location);
       this.map.setZoom(14);
 
-      this.marker.setPosition(place.geometry.location);
-      this.marker.setTitle(place.name);
+      this.addMarker(place.geometry.location, place.name);
+    } else {
+      console.error('No details available for input: ', place.name);
     }
+  }
+
+  toggleMapStyle() {
+    this.isCustomStyleApplied = !this.isCustomStyleApplied;
+    this.map.setOptions({
+      styles: this.isCustomStyleApplied
+        ? this.customStyles
+        : this.defaultStyles,
+    });
   }
 }
