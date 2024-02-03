36fd6e81a07dcdc82afc18224267e78abc74f456
IdoAviramMoveo <idoa@moveo.co.il>
2024-02-03 12:51:52 +0200

    Logout fix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
@@ -8,11 +8,15 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   email: string = '';
   errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.logout();
+  }
 
   onLogin(): void {
     if (this.authService.login(this.email)) {
